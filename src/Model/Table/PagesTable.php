5f270dded4281920985071ea596382c91caf4823
Ueyama Yuuki <minchobin@gmail.com>
2020-12-08 14:19:53 +0900

    no message

--- src/Model/Table/PagesTable.php
+++ src/Model/Table/PagesTable.php
@@ -12,25 +12,5 @@ class PagesTable extends Table
     {
         $this->addBehavior('Timestamp');
     }
-    public function beforeSave($event, $entity, $options)
-    {
-        if ($entity->isNew() && !$entity->slug) {
-            $sluggedTitle = Text::slug($entity->title);
-            // スラグをスキーマで定義されている最大長に調整
-            $entity->slug = substr($sluggedTitle, 0, 191);
-        }
-    }
-    public function validationDefault(Validator $validator): \Cake\Validation\Validator
-    {
-        $validator
-            ->minLength('title', 10)
-            ->maxLength('title', 255)
-            ->notEmpty('title')
-
-            ->notEmpty('body')
-            ->minLength('body', 10);
-
-
-        return $validator;
-    }
+    
 }
