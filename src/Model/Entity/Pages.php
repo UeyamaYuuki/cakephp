7570965efbfbda35c74654cd306d468e413e356f
Ueyama Yuuki <minchobin@gmail.com>
2020-12-08 13:58:10 +0900

    Pages.php追加

---
+++ src/Model/Entity/Pages.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+class Page extends Entity
+{
+    protected
+        $_accessible = [
+            '*' => true,
+            'id' => false,
+            'slug' => false
+        ];
+}
